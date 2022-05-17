ae82bec6df526e7d4a950d18caac834bfd241827
Ana Beatriz Carvalho <81018254+AnaBeatriz-Carvalho@users.noreply.github.com>
2022-05-17 18:54:25 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,26 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroFlutuante = 3.3;
+
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroFlutuante;
+
+console.log(novaOperacao);
+
+//NaN -> not a number
+
+const alura = 'Alura';
+console.log(alura*primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,37 @@
+const texto1 = 'Olá, mundo!';
+const texto2 = 'Olá, mundo';
+const senha = 'senhaSuperSegura456!';
+const stringDeNumeros = '34567';
+
+
+const citacao = 'Meu nome é ';
+const meuNome = "Ana";
+
+console.log(citacao + meuNome);
+
+
+//concatenação (+)
+
+
+console.log('--------------------------------------')
+//template string ou template literal
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+console.log('--------------------------------------')
+const cidade = 'belo horizonte';
+const input = 'Belo Horizonte';
+const inputMinusculo = input.toLowerCase();
+console.log(cidade === inputMinusculo);
+
+console.log('--------------------------------------')
+const Senha = 'minhasenha123';
+console.log('Minha senha possui '+ Senha.length + ' caracteres');
+
